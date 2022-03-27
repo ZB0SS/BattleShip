fb35787c270f89b27404fe69b5b79c0ca69559bf
ZB0SS <zbk0786@gamil.com>
2022-03-27 15:34:22 -0400

    Add Vertical Boats

--- main.cpp
+++ main.cpp
@@ -5,6 +5,20 @@
 #include <algorithm>
 using namespace std;
 
+int charToInt(char c) {
+    string holder;
+    holder.push_back(c);
+    return stoi(holder);
+}
+
+bool checkIfPosIsFilled(map<char, vector<char>> board, char x, int y) {
+    char val = board[x][y];
+    if (val == 'X') {
+        return true;
+    }
+    return false;
+}
+
 void printBoard(map<char, vector<char>>  board) {
     cout << "\t    0  1  2  3  4  5  6  7  8  9" << endl;
     for (auto pair : board) {
@@ -28,7 +42,7 @@ auto createBoard(){
     board.insert(pair<char, vector<char>>('G', row));
     board.insert(pair<char, vector<char>>('H', row));
     board.insert(pair<char, vector<char>>('I', row));
-
+    board.insert(pair<char, vector<char>>('J', row));
     printBoard(board);
     return board;
 }
@@ -42,70 +56,127 @@ string askUserForPos() {
 
 auto placeBoats(map<char, vector<char>> board) {
     // 2, 3, 3, 4, 5
-    vector<int> boats = {2, 3};
-    for (int boat : boats) {
+    vector<int> boats = {2, 3, 3, 4, 5};
+    for (int boat: boats) {
         string startingPlace;
         string endPlace;
         cout << "Where would you like to place your " << boat << " unit boat" << endl;
         cin >> startingPlace;
         cout << "Where would you like to place the end of your boat?" << endl;
         cin >> endPlace;
-
+        // Letters are the same F3 F4 F5
         if (startingPlace[0] == endPlace[0]) {
-            // Letters are the same F3 F4 F5
 
-            string holder;
-            // convert char -> string -> int
-            holder.push_back(startingPlace[1]);
-            int start = stoi(holder);
-            holder.clear();
-            holder.push_back(endPlace[1]);
-            int end = stoi(holder);
+            // F5 F1
+            int start = charToInt(startingPlace[1]);
+            int end = charToInt(endPlace[1]);
 
+            if (start > end) {
+                int holder = start;
+                start = end;
+                end = holder;
+            }
+
+            //J6 J7 J8 J9
+            if (abs(start - end) + 1 != boat) {
+                cout << "Not enough units provided";
+                exit(0);
+            }
             int looped = 0;
             bool c = true;
-            for (int i = start;  i <= end; i++) {
+            for (int i = start; i <= end; i++) {
                 if (c) {
-                    board[startingPlace[0]][i] = 'X';
+                    if (!checkIfPosIsFilled(board, startingPlace[0], i)) {
+                        board[startingPlace[0]][i] = 'X';
+                    }
+                    else {
+                        cout << "Boat overlaps with another boat";
+                        exit(0);
+                    }
+
                 }
-                looped ++;
+                looped++;
                 if (looped == boat) {
                     c = false;
                 }
-            }
-        }
-        else if (endPlace[1] == startingPlace[1]) {
-            // Numbers are the same
-        }
+            } // Numbers are the same letters are diffrent
+        } else if (endPlace[1] == startingPlace[1]) {
 
+            // TODO Check if letter is in the key
+            const int num = charToInt(startingPlace[1]);
+            char startLetter = startingPlace[0];
+            char endLetter = endPlace[0];
+            map<char, int> key = {
+                    {'A', 1},
+                    {'B', 2},
+                    {'C', 3},
+                    {'D', 4},
+                    {'E', 5},
+                    {'F', 6},
+                    {'G', 7},
+                    {'H', 8},
+                    {'I', 9},
+                    {'J', 10}
+            };
+            map<int, char> reversedKeyMap = {};
+            for (auto p: key) {
+                reversedKeyMap.insert(pair<int, char>(p.second, p.first));
+            }
+            int startVal = key[startLetter];
+            int endVal = key[endLetter];
+
+            if (startVal > endVal) {
+                int holder = startVal;
+                startVal = endVal;
+                endVal = holder;
+            }
+
+            // Correct Amount Of Units
+            if (abs(startVal - endVal) + 1 == boat) {
+                for (int i = startVal; i <= endVal; i++) {
+                    if (!checkIfPosIsFilled(board, reversedKeyMap[i], num)) {
+                        board[reversedKeyMap[i]][num] = 'X';
+                    } else {
+                        cout << "Boat overlaps with another boat";
+                        exit(0);
+                    }
+
+                }
+            }
+            else {
+                cout << "Too many units or too less units provided" << endl;
+                exit(0);
+            }
+
+        }
+        printBoard(board);
     }
     return board;
 }
-int main() {
-    vector<char> yPositions = {'A', 'B', 'C', 'D', 'E', 'G', 'H', 'I'};
-    map<char, vector<char>> firstBoard = createBoard();
-    // Get the location
-//    string input = askUserForPos();
-//    string numInput;
-//    char yPos = input[0];
-//    numInput.push_back(input[1]);
-//    int xPos = stoi(numInput);
-//    firstBoard[yPos][xPos] = 'X';
-    firstBoard = placeBoats(firstBoard);
-    printBoard(firstBoard);
 
-    return 0;
-}
+    int main() {
+        string holder;
+        vector<char> yPositions = {'A', 'B', 'C', 'D', 'E', 'G', 'H', 'I', 'J'};
+        map<char, vector<char>> firstBoard = createBoard();
+
+        firstBoard = placeBoats(firstBoard);
+        cout << "Type any key for the second player to begin: ";
+        cin >> holder;
+        cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+        map<char, vector<char>> secondBoard = createBoard();
+        secondBoard = placeBoats(secondBoard);
+        return 0;
+    }
 
 
 /*      1 2 3 4 5 6 7 8 9 10
     A   x x x x x x x x x x
-    B   x x x x x x x x x x 
+    B   x x x x x x x x x x
     C   x x x x x x x x x x
     D   x x x x x x x x x x
     E   x x x x x x x x x x
     F   x x x x x x x x x x
     G   x x x x x x x x x x
     H   x x x x x x x x x x
-    I   x x x x x x x x x x 
+    I   x x x x x x x x x x
 */
